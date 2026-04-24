a3238f0b0ec2352f695294af4e78d9e84eb4a6a9
KRABIKlol <egortryanzin07@gmail.com>
2026-04-24 11:53:50 +0300

    6-8

--- Legacy_of_Steel/Assets/Scripts/GrabParenter.cs
+++ Legacy_of_Steel/Assets/Scripts/GrabParenter.cs
@@ -4,5 +4,15 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class GrabParenter : MonoBehaviour
 {
-  
+    public void OnGrab(SelectEnterEventArgs args)
+    {
+        Debug.Log("Взял");
+        args.interactableObject.transform.SetParent(args.interactorObject.transform);
+    }
+
+    public void OnUngrab(SelectExitEventArgs args)
+    {
+        Debug.Log("Отпустил");
+        args.interactableObject.transform.SetParent(null);
+    }
 }
