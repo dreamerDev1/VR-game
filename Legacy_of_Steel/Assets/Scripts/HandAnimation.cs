106a5f95def1b8c7ad5b4af349fbad1026ec6ebd
YanaDev06 <yana.bystrova06bystrova@yandex.ru>
2026-04-24 08:03:52 +0300

    6

---
+++ Legacy_of_Steel/Assets/Scripts/HandAnimation.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Readers;
+
+public class HandAnimation : MonoBehaviour
+{
+
+    [SerializeField]
+    XRInputValueReader<float> m_TriggerInput;
+
+    [SerializeField]
+    XRInputValueReader<float> m_GripInput;
+
+    [SerializeField] Animator animator;
+
+    private void Update()
+    {
+        //Debug.Log(m_TriggerInput.ReadValue() + " " + m_GripInput.ReadValue());
+        animator.SetFloat("Trigger", m_TriggerInput.ReadValue());
+        animator.SetFloat("Grip", m_GripInput.ReadValue());
+    }
+}
